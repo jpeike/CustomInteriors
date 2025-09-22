5aa489c1629abf5efda1131ef13596194afc20d4
Wilsont0554 <wilsont0554@my.uwstout.edu>
2025-09-22 16:13:45 -0500

    Fized syntax of Customers table to better match examples.
    Added customers backend object, serivces, interfaces, and controllers

---
+++ Database/Tables/Customers.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Customer] (
+	[CustomerId] INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+	[FirstName] NVARCHAR(100) NOT NULL,
+	[LastName]	NVARCHAR(100) NOT NULL,
+	[CustomerType] NVARCHAR(100) NOT NULL,
+	[PrefferedContactMethod] NVARCHAR(100),
+	[CompanyName] NVARCHAR(100),
+	[Status] NVARCHAR(100),
+	[CustomerNotes] TEXT
+);
+GO
+
---
+++ Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,51 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public CustomerRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Customer?> GetCustomerById(int id)
+    {
+        return await _dbContext.Customers.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllCustomers()
+    {
+        return await _dbContext.Customers.ToListAsync();
+    }
+
+    public async Task<Customer> AddCustomer(Customer user)
+    {
+        _dbContext.Customers.Add(user);
+        await _dbContext.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task UpdateCustomer(Customer user)
+    {
+        _dbContext.Customers.Update(user);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> DeleteCustomer(int id)
+    {
+        Customer? user = await _dbContext.Customers.FindAsync(id);
+        if (user != null)
+        {
+            _dbContext.Remove(user);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+}
+
