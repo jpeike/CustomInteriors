d6a225ce1726890bff8f044e6a7e319edae1bffb
Joey Peikert <joeypeikert@gmail.com>
2025-10-20 10:17:35 -0500

    logout works, added logout to home page for testng, can remove anytime.

--- WebUI/src/views/HomePage.vue
+++ WebUI/src/views/HomePage.vue
@@ -1,7 +1,14 @@
 <template>
   <h1>Home Page</h1>
+  <button @click="logout">Log me out please</button>
 </template>
 
 <script setup lang="ts">
-import Login from './components/Login.vue'
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
+
+function logout() {
+  auth.logout()
+}
 </script>
