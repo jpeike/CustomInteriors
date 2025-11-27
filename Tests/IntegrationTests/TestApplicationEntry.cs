89c6c2d0e3132f7142fbf35d522b4d2e30e9ae5c
Andrew <schmidta0742@my.uwstout.edu>
2025-11-26 23:45:00 -0600

    got basic integration test working

---
+++ Tests/IntegrationTests/TestApplicationEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Program = Web.Program;
+
+namespace Testing.IntegrationTests;
+
+public class TestApplicationEntry : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // use in memory db for testing, set in web program.cs
+        builder.UseEnvironment("Testing");
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,146 +1,158 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Core;
 using Infrastructure;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using Web;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Web;
 
-
-// 1. Add JWT bearer authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+public class Program
+{
+    public static void Main(string[] args)
     {
-        options.Authority = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_JPMU56Ifb";
-        options.Audience = "574mvan5pjeoifpt063t473se6";
+        var builder = WebApplication.CreateBuilder(args);
 
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_JPMU56Ifb",
-
-            ValidateAudience = false,
-
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true
-        };
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
+        // 1. Add JWT bearer authentication
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
             {
-                var clientIdClaim = context.Principal?.Claims
-                    .FirstOrDefault(c => c.Type == "client_id")?.Value;
+                options.Authority = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_JPMU56Ifb";
+                options.Audience = "574mvan5pjeoifpt063t473se6";
 
-                if (clientIdClaim != "574mvan5pjeoifpt063t473se6")
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    context.Fail("Invalid client_id");
-                }
+                    ValidateIssuer = true,
+                    ValidIssuer = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_JPMU56Ifb",
 
-                return Task.CompletedTask;
-            }
-        };
-    });
+                    ValidateAudience = false,
 
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var clientIdClaim = context.Principal?.Claims
+                            .FirstOrDefault(c => c.Type == "client_id")?.Value;
 
+                        if (clientIdClaim != "574mvan5pjeoifpt063t473se6")
+                        {
+                            context.Fail("Invalid client_id");
+                        }
 
-// 2. Add role-based authorization policies
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy(Policies.AdminOnly, policy =>
-        policy.RequireClaim("cognito:groups", Roles.Admin));
-    options.AddPolicy(Policies.ManagerOrAdmin, policy =>
-        policy.RequireAssertion(context =>
-            context.User.HasClaim("cognito:groups", Roles.Admin) ||
-            context.User.HasClaim("cognito:groups", Roles.Manager)));
-});
+                        return Task.CompletedTask;
+                    }
+                };
+            });
 
-
-// Add services to the container.
-builder.Services.AddScoped<IAddressRepository, AddressRepository>();
-builder.Services.AddScoped<IAddressService, AddressService>();
-
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-
-builder.Services.AddScoped<IEmailRepository, EmailRepository>();
-builder.Services.AddScoped<IEmailService, EmailService>();
-
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-
-builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
-builder.Services.AddScoped<IInvoiceService, InvoiceService>();
-
-builder.Services.AddScoped<IInvoiceItemRepository, InvoiceItemRepository>();
-builder.Services.AddScoped<IInvoiceItemService, InvoiceItemService>();
-
-builder.Services.AddScoped<IJobRepository, JobRepository>();
-builder.Services.AddScoped<IJobService, JobService>();
-
-builder.Services.AddScoped<IJobAssignmentRepository, JobAssignmentRepository>();
-builder.Services.AddScoped<IJobAssignmentService, JobAssignmentService>();
-
-builder.Services.AddScoped<IJobInvoiceRepository, JobInvoiceRepository>();
-builder.Services.AddScoped<IJobInvoiceService, JobInvoiceService>();
-
-builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
-
-builder.Services.AddScoped<IPhoneRepository, PhoneRepository>();
-builder.Services.AddScoped<IPhoneService, PhoneService>();
-
-builder.Services.AddScoped<IQuoteRequestRepository, QuoteRequestRepository>();
-builder.Services.AddScoped<IQuoteRequestService, QuoteRequestService>();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy =>
+        // 2. Add role-based authorization policies
+        builder.Services.AddAuthorization(options =>
         {
-            policy.WithOrigins("http://localhost:5173") // Vite dev server
-                .AllowAnyHeader()
-                .AllowAnyMethod();
+            options.AddPolicy(Policies.AdminOnly, policy =>
+                policy.RequireClaim("cognito:groups", Roles.Admin));
+            options.AddPolicy(Policies.ManagerOrAdmin, policy =>
+                policy.RequireAssertion(context =>
+                    context.User.HasClaim("cognito:groups", Roles.Admin) ||
+                    context.User.HasClaim("cognito:groups", Roles.Manager)));
         });
-});
 
+        // Add services to the container.
+        builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+        builder.Services.AddScoped<IAddressService, AddressService>();
 
-var app = builder.Build();
+        builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+        builder.Services.AddScoped<ICustomerService, CustomerService>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+        builder.Services.AddScoped<IEmailService, EmailService>();
+
+        builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+        builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+        builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+
+        builder.Services.AddScoped<IInvoiceItemRepository, InvoiceItemRepository>();
+        builder.Services.AddScoped<IInvoiceItemService, InvoiceItemService>();
+
+        builder.Services.AddScoped<IJobRepository, JobRepository>();
+        builder.Services.AddScoped<IJobService, JobService>();
+
+        builder.Services.AddScoped<IJobAssignmentRepository, JobAssignmentRepository>();
+        builder.Services.AddScoped<IJobAssignmentService, JobAssignmentService>();
+
+        builder.Services.AddScoped<IJobInvoiceRepository, JobInvoiceRepository>();
+        builder.Services.AddScoped<IJobInvoiceService, JobInvoiceService>();
+
+        builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+        builder.Services.AddScoped<IPaymentService, PaymentService>();
+
+        builder.Services.AddScoped<IPhoneRepository, PhoneRepository>();
+        builder.Services.AddScoped<IPhoneService, PhoneService>();
+
+        builder.Services.AddScoped<IQuoteRequestRepository, QuoteRequestRepository>();
+        builder.Services.AddScoped<IQuoteRequestService, QuoteRequestService>();
+
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IUserService, UserService>();
+
+        if (builder.Environment.IsEnvironment("Testing"))
+        {
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+        }
+        else
+        {
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowFrontend",
+                policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173") // Vite dev server
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        //  Serve Vue static files
+        app.UseDefaultFiles(); // Looks for index.html
+        app.UseStaticFiles(); // Serves js/css/images
+
+        app.UseHttpsRedirection();
+
+        app.UseCors("AllowFrontend");
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
+    }
 }
-
-//  Serve Vue static files
-app.UseDefaultFiles(); // Looks for index.html
-app.UseStaticFiles(); // Serves js/css/images
-
-
-app.UseHttpsRedirection();
-
-app.UseCors("AllowFrontend");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
